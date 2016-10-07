d0b95a9f8afa63ab1400238538b22a5a57fe621f
techmusau <maxmusau@gmail.com>
2016-10-07 06:17:30 +0300

    Signed-off-by: techmusau <maxmusau@gmail.com>

--- client.py
+++ client.py
@@ -1,13 +1,23 @@
 import socket
+import sys
   
-TCP_IP = '172.16.6.150'
+TCP_IP = '192.168.0.12'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
  
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+except socket.error, (value,message):
+    if s:
+        s.close()
+        print 'could not open socket: '+ message
+        sys.exit(1)
+print 'Connected'
 s.send(MESSAGE)
+print 'sending message', MESSAGE
 data = s.recv(BUFFER_SIZE)
 s.close()
-print "Server received data:", data
+print 'waiting for response..'
+print "Response received data:", data
